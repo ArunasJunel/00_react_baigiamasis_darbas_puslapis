83c4a42315e9e747e2631d4cc151e45881db2779
A.Junel <arunas.junel.code@gmail.com>
2023-09-07 16:27:01 +0300

    Routes + HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ShopsPage from "./pages/ShopsPage";
+import AddShopPage from "./pages/AddShopPage";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/shops" element={<ShopsPage />} />
+        <Route path="/add-shop" element={<AddShopPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
