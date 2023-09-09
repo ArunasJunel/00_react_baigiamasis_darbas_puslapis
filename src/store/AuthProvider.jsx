2ae0c95ea23954ed2bfc7f6a78da261eb8252e8a
A.Junel <arunas.junel.code@gmail.com>
2023-09-09 12:33:27 +0300

    onAuthStateChanged + userUid

--- src/store/AuthProvider.jsx
+++ src/store/AuthProvider.jsx
@@ -1,8 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
   email: "",
   isUserLoggedIn: "",
+  userUid: "",
 });
 
 AuthContext.displayName = "Auth";
@@ -10,12 +12,27 @@ AuthContext.displayName = "Auth";
 export default function AuthProvider(props) {
   const [user, setUser] = useState({});
   const email = user?.email;
+  const userUid = user?.uid;
   const isUserLoggedIn = !!email;
 
   const ctx = {
     email: email,
     isUserLoggedIn: isUserLoggedIn,
+    userUid: userUid,
   };
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("Login successful");
+        setUser(user);
+      } else {
+        console.log("Logout");
+        setUser({});
+      }
+    });
+  }, []);
   return (
     <AuthContext.Provider value={ctx}>{props.children}</AuthContext.Provider>
   );
